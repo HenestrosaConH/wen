79d93f20871e4ffac2c9329d828734fa1deb9e84
josecarlosLH <josecarlos.lopez@internaliagroup.com>
2021-06-10 10:11:45 +0200

    v1.0: Release version

--- app/src/main/java/com/example/videomeeting/activities/UsersListCallsActivity.java
+++ app/src/main/java/com/example/videomeeting/activities/UsersListCallsActivity.java
@@ -18,7 +18,6 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.videomeeting.R;
 import com.example.videomeeting.adapters.recyclerviews.UsersListCallsAdapter;
 import com.example.videomeeting.listeners.OnMultipleCallsListener;
-import com.example.videomeeting.models.Contact;
 import com.example.videomeeting.models.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -30,6 +29,7 @@ import com.trendyol.bubblescrollbarlib.BubbleScrollBar;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import static com.example.videomeeting.utils.Constants.INTENT_CALL_TYPE;
@@ -37,8 +37,7 @@ import static com.example.videomeeting.utils.Constants.INTENT_CALL_TYPE_VIDEO;
 import static com.example.videomeeting.utils.Constants.CURRENT_USER;
 import static com.example.videomeeting.utils.Constants.INTENT_ARE_MULTIPLE_SELECTED_USERS;
 import static com.example.videomeeting.utils.Constants.INTENT_SELECTED_USERS;
-import static com.example.videomeeting.utils.Constants.KEY_COLLECTION_CONTACTED_USER;
-import static com.example.videomeeting.utils.Constants.KEY_COLLECTION_USER;
+import static com.example.videomeeting.utils.Constants.KEY_COLLECTION_USERS;
 
 public class UsersListCallsActivityOn extends AppCompatActivity implements OnMultipleCallsListener {
 
@@ -59,50 +58,37 @@ public class UsersListCallsActivityOn extends AppCompatActivity implements OnMul
 
         errorMessageTV = findViewById(R.id.errorMessageTV);
         noContactsCV = findViewById(R.id.noContactsCV);
-        BubbleScrollBar scrollBar = findViewById(R.id.scrollbar);
+        setupRV();
+        getContacts();
+    }
 
+    private void setupRV() {
         usersList = new ArrayList<>();
         usersListCallsAdapter = new UsersListCallsAdapter(UsersListCallsActivityOn.this, usersList, this);
         allUsersRV = findViewById(R.id.allUsersRV);
         allUsersRV.setLayoutManager(new LinearLayoutManager(UsersListCallsActivityOn.this));
         allUsersRV.setAdapter(usersListCallsAdapter);
+        BubbleScrollBar scrollBar = findViewById(R.id.scrollbar);
         scrollBar.attachToRecyclerView(allUsersRV);
         scrollBar.setBubbleTextProvider(i -> usersListCallsAdapter.userList.get(i).getUserName()); //dunno
-
-        getContacts();
     }
 
     private void getContacts() {
-        FirebaseDatabase.getInstance().getReference(KEY_COLLECTION_CONTACTED_USER)
-                .child(CURRENT_USER.getId())
-                .addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.getValue() == null) {
-                            noContactsCV.setVisibility(View.VISIBLE);
-                        } else {
-                            usersList.clear();
-                            for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-                                if (dataSnapshot.getValue(Contact.class).isContact()) {
-                                    fillUserList(dataSnapshot);
-                                } else {
-                                    checkUserList();
-                                }
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        errorMessageTV.setText(String.format("%s", getString(R.string.no_users_available)));
-                        changeViewsVisibility(View.GONE, View.VISIBLE, View.GONE);
-                    }
-                });
+        if (CURRENT_USER.getContactedUsers() != null) {
+            usersList.clear();
+            for (Map.Entry<String, Boolean> contactedUser : CURRENT_USER.getContactedUsers().entrySet()) {
+                if (contactedUser.getValue()) {
+                    getContactData(contactedUser.getKey());
+                }
+            }
+        } else {
+            changeViewsVisibility(View.GONE, View.GONE, View.VISIBLE);
+        }
     }
 
-    private void fillUserList(DataSnapshot dataSnapshot) {
-        FirebaseDatabase.getInstance().getReference(KEY_COLLECTION_USER)
-                .child(dataSnapshot.getKey()) //userID
+    private void getContactData(String userID) {
+        FirebaseDatabase.getInstance().getReference(KEY_COLLECTION_USERS)
+                .child(userID)
                 .addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -120,16 +106,20 @@ public class UsersListCallsActivityOn extends AppCompatActivity implements OnMul
 
     private void checkUserList() {
         if (usersList.size() > 0) {
-            Collections.sort(usersList, (user, u1) -> {
-                String s1 = user.getUserName();
-                String s2 = u1.getUserName();
-                return s1.compareToIgnoreCase(s2);
-            });
+            sortUsersAlphab();
             usersListCallsAdapter.notifyDataSetChanged();
             changeViewsVisibility(View.VISIBLE, View.GONE, View.GONE);
         } else {
             changeViewsVisibility(View.GONE, View.GONE, View.VISIBLE);
         }
+    }
+
+    private void sortUsersAlphab() {
+        Collections.sort(usersList, (user, u1) -> {
+            String s1 = user.getUserName();
+            String s2 = u1.getUserName();
+            return s1.compareToIgnoreCase(s2);
+        });
     }
 
     private void changeViewsVisibility(int allUsersVis, int errorMessageVis, int noContactsVis) {
